ab6250d56eb909668989efd57d88dbcc11303fb1
Munoz Daniel <dmunoz@micracel.com.ar>
2021-05-04 17:17:00 -0300

    plainToClass

--- src/middleware/validation.middleware.ts
+++ src/middleware/validation.middleware.ts
@@ -1,9 +1,12 @@
-import { plainToClass } from 'class-transformer';
+// import { plainToClass } from 'class-transformer';
 // import { validate, ValidationError } from 'class-validator';
 import { RequestHandler } from 'express';
 import HttpException from '../exceptions/HttpException';
 import pkg, { ValidationError } from 'class-validator';
 const { validate } = pkg;
+import classTransformer from 'class-transformer';
+const { plainToClass } = classTransformer;
+
 function validationMiddleware<T>(type: any, skipMissingProperties = false): RequestHandler {
   return (req, res, next) => {
     validate(plainToClass(type, req.body), { skipMissingProperties }).then((errors: ValidationError[]) => {
