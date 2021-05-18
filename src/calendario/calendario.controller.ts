076431f4e83ca76d97e52511b59fa5c8a0c6d2af
Munoz Daniel <dmunoz@micracel.com.ar>
2021-05-18 18:44:56 -0300

    calendario crear nuevo metodo

--- src/calendario/calendario.controller.ts
+++ src/calendario/calendario.controller.ts
@@ -27,8 +27,149 @@ class CalendarioController implements Controller {
     this.router.get(`${this.path}/migrar`, this.migrar);
     this.router.get(`${this.path}`, this.obtenerCalendario);
     this.router.get(`${this.path}/por-ciclo/:ciclo`, this.obtenerCalendarioPorCiclo);
-    this.router.post(`${this.path}`, this.crearCalendario);
+    this.router.post(`${this.path}`, this.crearCalendarioNuevo);
   }
+  private crearCalendarioNuevo = async (request: Request, response: Response, next: NextFunction) => {
+    const now = new Date();
+    const hoy = new Date(moment(now).utc().format('YYYY-MM-DD'));
+    const cicloLectivoActual = await this.cicloLectivo.findOne({ anio: moment().year() });
+    const existentes = await this.calendario.aggregate([
+      {
+        $lookup: {
+          from: 'ciclolectivos',
+          localField: 'cicloLectivo',
+          foreignField: '_id',
+          as: 'cicloLectivo',
+        },
+      },
+      {
+        $unwind: {
+          path: '$cicloLectivo',
+        },
+      },
+      {
+        $match: {
+          'cicloLectivo._id': ObjectId(cicloLectivoActual._id),
+        },
+      },
+    ]);
+    const calendarioEliminado = await Promise.all(
+      existentes.map(async (x: any, index: number) => {
+        const eliminados = await this.calendario.findByIdAndDelete(x._id);
+      })
+    );
+    // Obtengo por parametro la fecha de inicio de clases
+    console.log('calendarioEliminado', calendarioEliminado);
+    const fechaFinal = request.body.fechaFinal;
+    // cicloLectivoActual
+    const calendarioNuevo = [];
+    let normal = true;
+    let fechaInicio = moment(request.body.fechaInicio, 'YYYY-MM-DD').utc(); // .add(-1, 'day')
+    //let fechaInicio = moment(request.body.fechaInicio).utc();
+    console.log('UINUI', request.body.fechaInicio);
+    console.log('UINUI', fechaFinal);
+    console.log('UINUI', moment(fechaFinal, 'YYYY-MM-DD').utc().isSameOrAfter(fechaInicio));
+    let miercolesSi = true;
+    while (moment(fechaFinal, 'YYYY-MM-DD').utc().isSameOrAfter(fechaInicio)) {
+      const fechaNombre = moment.utc(fechaInicio).format('dddd').toLowerCase();
+      let fecha = new Date(moment(fechaInicio).utc().format('YYYY-MM-DD'));
+      if (fechaNombre === 'monday') {
+        calendarioNuevo.push({
+          cicloLectivo: cicloLectivoActual,
+          comisionA: 1,
+          comisionB: 1,
+          comisionC: 1,
+          comisionD: 1,
+          comisionE: 0,
+          comisionF: 0,
+          comisionG: 0,
+          comisionH: 0,
+          fecha,
+          fechaCreacion: hoy,
+          activo: true,
+        });
+      }
+      if (fechaNombre === 'tuesday') {
+        calendarioNuevo.push({
+          cicloLectivo: cicloLectivoActual,
+          comisionA: 1,
+          comisionB: 1,
+          comisionC: 1,
+          comisionD: 1,
+          comisionE: 0,
+          comisionF: 0,
+          comisionG: 0,
+          comisionH: 0,
+          fecha,
+          fechaCreacion: hoy,
+          activo: true,
+        });
+      }
+      if (fechaNombre === 'wednesday') {
+        calendarioNuevo.push({
+          cicloLectivo: cicloLectivoActual,
+          comisionA: miercolesSi ? 1 : 0,
+          comisionB: miercolesSi ? 1 : 0,
+          comisionC: miercolesSi ? 1 : 0,
+          comisionD: miercolesSi ? 1 : 0,
+          comisionE: !miercolesSi ? 1 : 0,
+          comisionF: !miercolesSi ? 1 : 0,
+          comisionG: !miercolesSi ? 1 : 0,
+          comisionH: !miercolesSi ? 1 : 0,
+          fecha,
+          fechaCreacion: hoy,
+          activo: true,
+        });
+      }
+      if (fechaNombre === 'thursday') {
+        calendarioNuevo.push({
+          cicloLectivo: cicloLectivoActual,
+          comisionA: 0,
+          comisionB: 0,
+          comisionC: 0,
+          comisionD: 0,
+          comisionE: 1,
+          comisionF: 1,
+          comisionG: 1,
+          comisionH: 1,
+          fecha,
+          fechaCreacion: hoy,
+          activo: true,
+        });
+      }
+      if (fechaNombre === 'friday') {
+        calendarioNuevo.push({
+          cicloLectivo: cicloLectivoActual,
+          comisionA: 0,
+          comisionB: 0,
+          comisionC: 0,
+          comisionD: 0,
+          comisionE: 1,
+          comisionF: 1,
+          comisionG: 1,
+          comisionH: 1,
+          fecha,
+          fechaCreacion: hoy,
+          activo: true,
+        });
+      }
+      miercolesSi = !miercolesSi;
+      fechaInicio = moment(fechaInicio).utc().add(1, 'day');
+      // ============
+
+      //    fechaInicio = moment(fechaInicio).utc().add(1, 'day');
+    }
+    console.log('calendarioNuevo', calendarioNuevo);
+    try {
+      const saved = await this.calendario.insertMany(calendarioNuevo);
+
+      response.send({ calendarioNuevo: saved, success: true });
+    } catch (e) {
+      // [ 'errors', '_message', 'message', 'name' ]
+      console.log('[ERROR]', e);
+      next(new HttpException(500, 'Problemas al insertar los registros'));
+    }
+  };
   private crearCalendario = async (request: Request, response: Response, next: NextFunction) => {
     const now = new Date();
     const hoy = new Date(moment(now).utc().format('YYYY-MM-DD'));
@@ -59,53 +200,63 @@ class CalendarioController implements Controller {
       })
     );
     // Obtengo por parametro la fecha de inicio de clases
-
+    console.log('calendarioEliminado', calendarioEliminado);
     const fechaFinal = request.body.fechaFinal;
     // cicloLectivoActual
     const calendarioNuevo = [];
     let normal = true;
-    let fechaInicio = moment(request.body.fechaInicio).utc().add(-1, 'day');
+    let fechaInicio = moment(request.body.fechaInicio, 'YYYY-MM-DD').utc().add(-1, 'day');
     //let fechaInicio = moment(request.body.fechaInicio).utc();
+    console.log('UINUI', request.body.fechaInicio);
+    console.log('UINUI', fechaFinal);
+    console.log('UINUI', moment(fechaFinal, 'YYYY-MM-DD').utc().isSameOrAfter(fechaInicio));
+    let miercolesSi = true;
     while (moment(fechaFinal, 'YYYY-MM-DD').utc().isSameOrAfter(fechaInicio)) {
       if (normal) {
         for (let i = 0; i < 3; i++) {
           fechaInicio = moment(fechaInicio).utc().add(1, 'day');
           if (moment(fechaFinal, 'YYYY-MM-DD').utc().isSameOrAfter(fechaInicio)) {
             let fecha = new Date(moment(fechaInicio).utc().format('YYYY-MM-DD'));
-            calendarioNuevo.push({
-              cicloLectivo: cicloLectivoActual,
-              comisionA: 1,
-              comisionB: 1,
-              comisionC: 1,
-              comisionD: 1,
-              comisionE: 0,
-              comisionF: 0,
-              comisionG: 0,
-              comisionH: 0,
-              fecha,
-              fechaCreacion: hoy,
-              activo: true,
-            });
+            const fechaNombre = moment.utc(fechaInicio).format('dddd').toLowerCase();
+            if (fechaNombre !== 'saturday' && fechaNombre !== 'sunday') {
+              calendarioNuevo.push({
+                cicloLectivo: cicloLectivoActual,
+                comisionA: 1,
+                comisionB: 1,
+                comisionC: 1,
+                comisionD: 1,
+                comisionE: 0,
+                comisionF: 0,
+                comisionG: 0,
+                comisionH: 0,
+                fecha,
+                fechaCreacion: hoy,
+                activo: true,
+              });
+            }
           }
         }
         for (let i = 0; i < 2; i++) {
           fechaInicio = moment(fechaInicio).utc().add(1, 'day');
           if (moment(fechaFinal, 'YYYY-MM-DD').utc().isSameOrAfter(fechaInicio)) {
             let fecha = new Date(moment(fechaInicio).utc().format('YYYY-MM-DD'));
-            calendarioNuevo.push({
-              cicloLectivo: cicloLectivoActual,
-              comisionA: 0,
-              comisionB: 0,
-              comisionC: 0,
-              comisionD: 0,
-              comisionE: 1,
-              comisionF: 1,
-              comisionG: 1,
-              comisionH: 1,
-              fecha,
-              fechaCreacion: hoy,
-              activo: true,
-            });
+            const fechaNombre = moment.utc(fechaInicio).format('dddd').toLowerCase();
+            if (fechaNombre !== 'saturday' && fechaNombre !== 'sunday') {
+              calendarioNuevo.push({
+                cicloLectivo: cicloLectivoActual,
+                comisionA: 0,
+                comisionB: 0,
+                comisionC: 0,
+                comisionD: 0,
+                comisionE: 1,
+                comisionF: 1,
+                comisionG: 1,
+                comisionH: 1,
+                fecha,
+                fechaCreacion: hoy,
+                activo: true,
+              });
+            }
           }
         }
       } else {
@@ -113,50 +264,57 @@ class CalendarioController implements Controller {
           fechaInicio = moment(fechaInicio).utc().add(1, 'day');
           if (moment(fechaFinal, 'YYYY-MM-DD').utc().isSameOrAfter(fechaInicio)) {
             let fecha = new Date(moment(fechaInicio).utc().format('YYYY-MM-DD'));
-            calendarioNuevo.push({
-              cicloLectivo: cicloLectivoActual,
-              comisionA: 1,
-              comisionB: 1,
-              comisionC: 1,
-              comisionD: 1,
-              comisionE: 0,
-              comisionF: 0,
-              comisionG: 0,
-              comisionH: 0,
-              fecha,
-              fechaCreacion: hoy,
-              activo: true,
-            });
+            const fechaNombre = moment.utc(fechaInicio).format('dddd').toLowerCase();
+            if (fechaNombre !== 'saturday' && fechaNombre !== 'sunday') {
+              calendarioNuevo.push({
+                cicloLectivo: cicloLectivoActual,
+                comisionA: 1,
+                comisionB: 1,
+                comisionC: 1,
+                comisionD: 1,
+                comisionE: 0,
+                comisionF: 0,
+                comisionG: 0,
+                comisionH: 0,
+                fecha,
+                fechaCreacion: hoy,
+                activo: true,
+              });
+            }
           }
         }
         for (let i = 0; i < 3; i++) {
           fechaInicio = moment(fechaInicio).utc().add(1, 'day');
           if (moment(fechaFinal, 'YYYY-MM-DD').utc().isSameOrAfter(fechaInicio)) {
             let fecha = new Date(moment(fechaInicio).utc().format('YYYY-MM-DD'));
-            calendarioNuevo.push({
-              cicloLectivo: cicloLectivoActual,
-              comisionA: 0,
-              comisionB: 0,
-              comisionC: 0,
-              comisionD: 0,
-              comisionE: 1,
-              comisionF: 1,
-              comisionG: 1,
-              comisionH: 1,
-              fecha,
-              fechaCreacion: hoy,
-              activo: true,
-            });
+            const fechaNombre = moment.utc(fechaInicio).format('dddd').toLowerCase();
+            if (fechaNombre !== 'saturday' && fechaNombre !== 'sunday') {
+              calendarioNuevo.push({
+                cicloLectivo: cicloLectivoActual,
+                comisionA: 0,
+                comisionB: 0,
+                comisionC: 0,
+                comisionD: 0,
+                comisionE: 1,
+                comisionF: 1,
+                comisionG: 1,
+                comisionH: 1,
+                fecha,
+                fechaCreacion: hoy,
+                activo: true,
+              });
+            }
           }
         }
       }
       normal = !normal;
       //    fechaInicio = moment(fechaInicio).utc().add(1, 'day');
     }
+    console.log('calendarioNuevo', calendarioNuevo);
     try {
-      const saved = await this.calendario.insertMany({ calendarioNuevo, success: true });
+      const saved = await this.calendario.insertMany(calendarioNuevo);
 
-      response.send(saved);
+      response.send({ calendarioNuevo: saved, success: true });
     } catch (e) {
       // [ 'errors', '_message', 'message', 'name' ]
       console.log('[ERROR]', e);
