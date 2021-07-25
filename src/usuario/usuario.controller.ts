395ef327fe2231f21a4389e1c7c4c36d5c03a572
Munoz Daniel <dmunoz@micracel.com.ar>
2021-07-24 22:25:31 -0300

    link

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -246,10 +246,7 @@ class UsuarioController implements Controller {
           subject: 'Recuperar Contraseña',
           params: {
             code,
-            link:
-              ENTORNO === 'desarrollo'
-                ? `http://localhost:4200/auth/reset/${usuario._id}`
-                : `http://app.cet30.edu.ar/auth/reset/${usuario._id}`,
+            link: ENTORNO === 'desarrollo' ? `${usuario._id}` : `${usuario._id}`,
           },
           templateId: 2,
           // textContent:
