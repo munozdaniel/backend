fdecaf18d5a97c84a1a843b6f96c8464f492d068
Munoz Daniel <dmunoz@micracel.com.ar>
2021-11-01 12:49:41 -0300

    tema 300 caracteres

--- src/temas/tema.dto.ts
+++ src/temas/tema.dto.ts
@@ -1,80 +1,69 @@
-import {
-  IsString,
-  IsOptional,
-  IsBoolean,
-  IsDateString,
-  MaxLength,
-  MinLength,
-  ValidateNested,
-  Min,
-  IsNumber,
-} from "class-validator";
-import IPlanillaTaller from "../planillaTaller/planillaTaller.interface";
+import { IsString, IsOptional, IsBoolean, IsDateString, MaxLength, MinLength, ValidateNested, Min, IsNumber } from 'class-validator';
+import IPlanillaTaller from '../planillaTaller/planillaTaller.interface';
 
 class CrearTemaDto {
   @ValidateNested()
   planillaTaller: IPlanillaTaller;
   @IsDateString()
   fecha: Date;
-  @IsString({ message: "El tema del día no fue ingresado" })
+  @IsString({ message: 'El tema del día no fue ingresado' })
   @MinLength(4, {
-    message: "El tema del día debe contener al menos 4 caracteres",
+    message: 'El tema del día debe contener al menos 4 caracteres',
   })
-  @MaxLength(100, {
-    message: "El tema del día debe contener 100 caracteres máximo",
+  @MaxLength(300, {
+    message: 'El tema del día debe contener 300 caracteres máximo',
   })
   temaDelDia: string;
-  @IsString({ message: "El tipo de desarrollo no fue ingresado" })
+  @IsString({ message: 'El tipo de desarrollo no fue ingresado' })
   @MinLength(4, {
-    message: "El tipo de desarrollo debe contener al menos 4 caracteres",
+    message: 'El tipo de desarrollo debe contener al menos 4 caracteres',
   })
-  @MaxLength(100, {
-    message: "El tipo de desarrollo debe contener 100 caracteres máximo",
+  @MaxLength(300, {
+    message: 'El tipo de desarrollo debe contener 300 caracteres máximo',
   })
   tipoDesarrollo: string;
-  @IsString({ message: "La proxima clase no fue ingresada" })
+  @IsString({ message: 'La proxima clase no fue ingresada' })
   @MinLength(4, {
-    message: "La proxima clase debe contener al menos 4 caracteres",
+    message: 'La proxima clase debe contener al menos 4 caracteres',
   })
-  @MaxLength(100, {
-    message: "La proxima clase debe contener 100 caracteres máximo",
+  @MaxLength(300, {
+    message: 'La proxima clase debe contener 300 caracteres máximo',
   })
   temasProximaClase: string;
   @IsNumber(
     { allowNaN: false },
     {
-      message: "El nro de clase debe ser numerico",
+      message: 'El nro de clase debe ser numerico',
     }
   )
-  @Min(0, { message: "El Nro de clase no puede ser menor a 0" })
+  @Min(0, { message: 'El Nro de clase no puede ser menor a 0' })
   nroClase: number;
-  @Min(0, { message: "La unidad no puede ser menor a 0" })
+  @Min(0, { message: 'La unidad no puede ser menor a 0' })
   @IsNumber(
     { allowNaN: false },
     {
-      message: "La unidad debe ser numerico",
+      message: 'La unidad debe ser numerico',
     }
   )
   unidad: number;
   @IsString({
-    message: "El caracter de la clase de taller no ha sido ingresado",
+    message: 'El caracter de la clase de taller no ha sido ingresado',
   })
   @MinLength(7, {
-    message: "El caracter de la clase de taller  es muy corto",
+    message: 'El caracter de la clase de taller  es muy corto',
   })
-  @MaxLength(100, {
-    message: "El caracter de la clase no puede superar los 100 caracteres",
+  @MaxLength(300, {
+    message: 'El caracter de la clase no puede superar los 300 caracteres',
   })
   caracterClase: string;
   @IsString({
-    message: "La observacion del jefe de taller no ha sido ingresado",
+    message: 'La observacion del jefe de taller no ha sido ingresado',
   })
   @MinLength(4, {
-    message: "La observacion del jefe de taller  es muy corto",
+    message: 'La observacion del jefe de taller  es muy corto',
   })
-  @MaxLength(100, {
-    message:
-      "La observacion del jefe de taller no puede superar los 100 caracteres",
+  @MaxLength(300, {
+    message: 'La observacion del jefe de taller no puede superar los 300 caracteres',
   })
   observacionJefe: string;
 
